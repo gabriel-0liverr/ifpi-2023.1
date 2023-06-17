538a1d2f283f0fe703199e9a868ad8889c4922c7
dev-gabriel-0liverr <popm4ker@gmail.com>
2023-06-17 09:42:12 -0300

    Merge branch 'main' of https://github.com/gabriel-0liverr/ifpi-2023.1

---
+++ estrutura-de-dados-2/Arv_Bin_Morris_01.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+struct NoArvore {
+    int valor;
+    NoArvore* esquerda;
+    NoArvore* direita;
+
+    NoArvore(int x) : valor(x), esquerda(nullptr), direita(nullptr) {}
+};
+
+void MorrisInOrder(NoArvore* raiz) {
+    NoArvore* atual = raiz;
+    while (atual != nullptr) {
+        if (atual->esquerda == nullptr) {
+            cout << atual->valor << " ";
+            atual = atual->direita;
+        } else {
+            NoArvore* predecessor = atual->esquerda;
+            while (predecessor->direita != nullptr && predecessor->direita != atual) {
+                predecessor = predecessor->direita;
+            }
+
+            if (predecessor->direita == nullptr) {
+                predecessor->direita = atual;
+                atual = atual->esquerda;
+            } else {
+                predecessor->direita = nullptr;
+                cout << atual->valor << " ";
+                atual = atual->direita;
+            }
+        }
+    }
+}
+
+NoArvore* inserir(NoArvore* raiz, int valor) {
+    if (raiz == nullptr) {
+        return new NoArvore(valor);
+    }
+
+    if (valor < raiz->valor) {
+        raiz->esquerda = inserir(raiz->esquerda, valor);
+    } else {
+        raiz->direita = inserir(raiz->direita, valor);
+    }
+
+    return raiz;
+}
+
+int main() {
+    NoArvore* raiz = nullptr;
+    raiz = inserir(raiz, 4);
+    raiz = inserir(raiz, 2);
+    raiz = inserir(raiz, 6);
+    raiz = inserir(raiz, 1);
+    raiz = inserir(raiz, 3);
+    raiz = inserir(raiz, 5);
+    raiz = inserir(raiz, 7);
+
+    cout << "Percurso In-Order: ";
+    MorrisInOrder(raiz);
+    cout << endl;
+
+    return 0;
+}
